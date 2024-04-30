13859e933f9af255904a6373d3ca94a0e89e6e09
ekkanikanti <143454787+ekkanikanti@users.noreply.github.com>
2024-04-30 17:55:54 -0400

    Song data

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: 'Mind is a Prison',
+        path: 'music/MindPrison.mp3',
+        artist: 'Alec Benjamin',
+        cover: 'images/cover1.jpg'
+    },
+    {
+        name: 'Undertale Asgore Theme',
+        path: 'music/Asgore.mp3',
+        artist: 'Banjo Guy Ollie',
+        cover: 'images/cover2.jpg'
+    },
+    {
+        name: 'Senorita',
+        path: 'music/Senorita.mp3',
+        artist: 'Camila Cabello & Shawn Mendes',
+        cover: 'images/cover3.png'
+    },
+    {
+        name: 'Fireflies',
+        path: 'music/Fireflies.mp3',
+        artist: 'Owl City',
+        cover: 'images/cover4.jpg'
+    }
+]
